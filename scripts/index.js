006d465f7e376ff13b512357ff7052ac51e16e91
George Chetyrin <klon84@gmail.com>
2020-06-20 22:27:51 +0300

    Правки в проектной работе Место

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,32 @@
-const popup = document.querySelector('.popup')
-const popupOpenButton = document.querySelector('.profile__open-popup')
-const popupCloseButton = popup.querySelector('.popup__close')
+const popup = document.querySelector('.popup');
+const popupOpenButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = popup.querySelector('.popup__close');
+
+const profile = document.querySelector('.profile');
+const formElement = document.querySelector('.popup__form');
+
+const nameInput = formElement.querySelector('.popup__profile-title');
+const jobInput = formElement.querySelector('.popup__profile-subtitle');
+const name = profile.querySelector('.profile__title');
+const job = profile.querySelector('.profile__subtitle');
 
 
-const popupToggle = function (event) {
-    popup.classList.toggle('popup_opened')
+function popupToggle() {
+    if (popup.classList.contains('popup_opened')) {
+        nameInput.value = name.textContent;
+        jobInput.value = job.textContent;
+    }
+    popup.classList.toggle('popup_opened');
 }
-
-
-popupOpenButton.addEventListener('click', popupToggle)
-popupCloseButton.addEventListener('click', popupToggle)
-
-
-const profile = document.querySelector('.profile')
-const formElement = document.querySelector('.popup__form')
-
-const nameInput = formElement.querySelector('.popup__profile-title')
-const jobInput = formElement.querySelector('.popup__profile-subtitle')
-const name = profile.querySelector('.profile__title')
-const job = profile.querySelector('.profile__subtitle')
-
-nameInput.value = 'Жак-Ив Кусто'
-jobInput.value = 'Исследователь океана'
-
-
-const formCloseButton = popup.querySelector('.popup__submit-button')
-formCloseButton.addEventListener('click', popupToggle)
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    name.textContent = nameInput.value
-    job.textContent = jobInput.value
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    popupToggle();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
